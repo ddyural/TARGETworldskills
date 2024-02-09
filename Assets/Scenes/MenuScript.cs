a0ccded5bac07e0a5f3104b1e505cb745f90bdff
ddyural <84669233+ddyural@users.noreply.github.com>
2024-02-09 16:45:18 +0300

    Initial commit
    + created main menu
    + a login and password check has been created
    , it switches to the corresponding screen

---
+++ Assets/Scenes/MenuScript.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject AdminScreen;
+    public GameObject TutorScreen;
+    public GameObject StudentScreen;
+    public GameObject inputField1;
+    public GameObject inputField2;
+
+    public TextMeshProUGUI user_name;
+    public TMP_InputField login_inputField;
+    public TMP_InputField password_inputField;
+
+    Dictionary<string, string> dictAdmins = new Dictionary<string, string>();
+
+    Dictionary<string, string> dictTutors = new Dictionary<string, string>();
+
+    Dictionary<string, string> dictStudents = new Dictionary<string, string>();
+
+    private string input;
+
+
+
+
+    //public string Login = "";
+
+    //public string Password = "";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dictAdmins.Add("Admin", "Admin");
+        HideAllScreens();
+        MainMenu.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HideAllScreens()
+    {
+        MainMenu.SetActive(false);
+        AdminScreen.SetActive(false);
+        TutorScreen.SetActive(false);
+        StudentScreen.SetActive(false);
+    }
+
+    public void OpenScreenAdmin()
+    {
+        HideAllScreens();
+        AdminScreen.SetActive(true);
+    }
+
+    public void OpenScreenTutor()
+    {
+        HideAllScreens();
+        TutorScreen.SetActive(true);
+    }
+
+    public void OpenScreenStudent()
+    {
+        HideAllScreens();
+        StudentScreen.SetActive(true);
+    }
+
+    public void LoginCheck()
+    {   
+        string Login = login_inputField.text;
+        string Password = password_inputField.text;
+
+        Debug.Log(Login);
+        Debug.Log(Password);
+
+        bool foundInDict1 = dictAdmins.ContainsKey(Login) && dictAdmins[Login] == Password;
+        bool foundInDict2 = dictTutors.ContainsKey(Login) && dictTutors[Login] == Password;
+        bool foundInDict3 = dictStudents.ContainsKey(Login) && dictStudents[Login] == Password;
+
+        if (foundInDict1)
+        {
+            Debug.Log($"Элемент с ключом {Login} и значением {Password} найден в первом словаре");
+            OpenScreenAdmin();
+        }
+        else if (foundInDict2)
+        {
+            OpenScreenTutor();
+            Debug.Log($"Элемент с ключом {Login} и значением {Password} найден во втором словаре");
+        }
+        else if (foundInDict3)
+        {
+            OpenScreenStudent();
+            Debug.Log($"Элемент с ключом {Login} и значением {Password} найден в третьем словаре");
+        }
+        else
+        {
+            Debug.Log($"Элемент с ключом {Login} и значением {Password} не найден ни в одном из словарей");
+        }
+        
+
+    }
+
+    public void HowWorksLoginProcess()
+    {
+        
+    }
+}
